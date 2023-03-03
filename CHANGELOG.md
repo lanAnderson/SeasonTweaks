9c921ecf9403e809d620c3667b15344bd3fddbf4
Ian Anderson <iananderson92@gmail.com>
2023-03-02 23:46:54 -0700

    ## [1.0.2] - 2023-03-02
    ### Changed
    
    - Updated to the latest SereneSeasons with Biome tags

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
+## [1.0.2] - 2023-03-02
+
+### Changed
+
+- Updated to the latest SereneSeasons with Biome tags
+
+
 ## [1.0.1] - 2023-02-22
 
 ### Added
--- src/main/java/club/iananderson/seasontweaks/mixin/tropicalBiomes.java
+++ src/main/java/club/iananderson/seasontweaks/mixin/tropicalBiomes.java
@@ -1,29 +1,7 @@
 package club.iananderson.seasontweaks.mixin;
 
-import club.iananderson.seasontweaks.config.Config;
-import net.minecraft.core.Holder;
-import net.minecraft.world.level.biome.Biome;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import sereneseasons.config.BiomeConfig;
-
-@Mixin(BiomeConfig.class)
 public class tropicalBiomes {
-
-    @Inject(
-            at = @At("HEAD"),
-            method = "usesTropicalSeasons(Lnet/minecraft/core/Holder;)Z",
-            cancellable = true,
-            remap = false
-    )
-
-    private static void usesTropicalSeasons(Holder<Biome> biome, CallbackInfoReturnable<Boolean> callback){
-        if(Config.disableTropicalSeasons.get()){
-            callback.setReturnValue(false);
-        }
+    public tropicalBiomes(){
     }
-
 
 }
